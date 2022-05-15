5357cb6c6bb31f6cd0b1c4f00dd14bd481c61cb8
danjab-jd <jd@pja.edu.pl>
2022-05-15 12:52:06 +0200

    DbFirst example

---
+++ DbFirst/DbFirst/Entities/CityDict.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DbFirst.Entities
+{
+    public partial class CityDict
+    {
+        public CityDict()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        public int IdCityDict { get; set; }
+        public string City { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
